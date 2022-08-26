ea6657376091af1d8d6feab028a525a08b87f359
OmarSabbah999 <omars@mail.tau.ac.il>
2022-08-26 16:51:01 +0300

    make it better

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
-// import dotenv from "dotenv"
+import dotenv from "dotenv"
 import postRoutes from "./routes/posts.js"
 import newsRoutes from "./routes/news.js"
-require("dotenv").config()
+// require("dotenv").config()
 const app = express()
-// dotenv.config()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () =>
+    app.listen(process.env.PORT || 5000, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
