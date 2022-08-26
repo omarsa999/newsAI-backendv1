b6cd89214de60bd55c144f5251956a5543708481
OmarSabbah999 <omars@mail.tau.ac.il>
2022-08-26 21:13:23 +0300

    recreating the news

--- controllers/news.js
+++ controllers/news.js
@@ -14,7 +14,7 @@ const router = express.Router()
 export const getNews = async (req, res) => {
   try {
     const news = await newsProvider()
-    res.status(200).json(news)
+    res.status(200).json(news.data)
   } catch (error) {
     res.status(404).json({ message: error.message })
   }
@@ -40,26 +40,5 @@ export const saveNews = async (req, res) => {
     res.status(404).json({ message: error.message })
   }
 }
-
-// export const getNews = async (req, res) => {
-//   try {
-//     const news = await newsProvider()
-//     news.data.articles.forEach((post) => {
-//       const newArticle = {
-//         title: post.title,
-//         data: post.summary,
-//         timestamp: post.author,
-//         thumbnail: post.media,
-//         header: "pass",
-//         images: "pass",
-//       }
-//       // i have to save them as array of json objects
-//     })
-//     console.log(news.data.articles)
-//     res.status(200).json(news.data.articles)
-//   } catch (error) {
-//     res.status(404).json({ message: error.message })
-//   }
-// }
 
 export default router
