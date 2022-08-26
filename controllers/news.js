4114d9f8bc77a9f49efcf405e8fe0b834a56bfc6
OmarSabbah999 <omars@mail.tau.ac.il>
2022-08-26 21:08:43 +0300

    recreating the news

--- controllers/news.js
+++ controllers/news.js
@@ -1,14 +1,41 @@
 import express from "express"
-
 import newsProvider from "../models/newsProvider.js"
+import axios from "axios"
+import data from "../data.json" assert { type: "json" }
+
+const url = "http://localhost:5000/posts"
 
 const router = express.Router()
+// pipline:
+//  1- get the news
+//  2- save to mongoDB
 
 // this function will comunicate with newsProvider.getNews function
 export const getNews = async (req, res) => {
   try {
     const news = await newsProvider()
     res.status(200).json(news)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const saveNews = async (req, res) => {
+  try {
+    // const data = await getNews()
+    for (const post of data) {
+      // const post = data[0]
+      const postData = {
+        title: post.title,
+        postBody: post.summary,
+        selectedFile: "pass",
+        creator: post.author,
+        tags: "pass",
+      }
+      const newPostData = await axios.post(url, postData)
+      console.log(postData, newPostData)
+    }
+    res.status(201)
   } catch (error) {
     res.status(404).json({ message: error.message })
   }
--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,10 @@
 import express from "express"
 
-import { getNews } from "../controllers/news.js"
+import { getNews, saveNews } from "../controllers/news.js"
 
 const router = express.Router()
 
 router.get("/", getNews)
+router.post("/", saveNews)
 
 export default router
