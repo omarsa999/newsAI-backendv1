e6da7f0d7809bd2e3261175986d6217f1a797183
OmarSabbah999 <omars@mail.tau.ac.il>
2022-08-26 16:39:34 +0300

    make it better 2

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 import postRoutes from "./routes/posts.js"
 import newsRoutes from "./routes/news.js"
-
+require("dotenv").config()
 const app = express()
-dotenv.config()
+// dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
