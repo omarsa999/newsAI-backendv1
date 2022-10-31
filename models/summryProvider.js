1a172d85f091bd0d896289205184f4dbb52d9ee8
OmarSabbah999 <omars@mail.tau.ac.il>
2022-10-31 18:49:53 +0200

    testing axios req

--- models/summryProvider.js
+++ models/summryProvider.js
@@ -2,12 +2,14 @@
 import axios from "axios"
 
 const summryProvider = async (txt) => {
+  const apiKey = "9f1028a0ddmsh45694ba7a9684d8p1485b6jsn84e6edc9723d"
+  // process.env.summryProviderAPI
   const options = {
     method: "POST",
     url: "https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite",
     headers: {
       "content-type": "application/json",
-      "X-RapidAPI-Key": process.env.summryProviderAPI,
+      "X-RapidAPI-Key": apiKey,
       "X-RapidAPI-Host":
         "rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com",
     },
